423878546fcbf720848c72c608d8ec77113c44ad
Nick Chrissomallis <nchrissomallis@comcast.net>
2023-12-11 13:15:23 -0500

    Add DefaultPlayer for instances where generic sounds can share a channel

--- SFXFunctions.gd
+++ SFXFunctions.gd
@@ -1,5 +1,10 @@
 extends Node
 
+@onready var default_player = $DefaultPlayer
+
 func play_sound_on_player(sound: AudioStream, player : AudioStreamPlayer):
 	player.stream = sound
 	player.play()
+
+func play_sound(sound: AudioStream):
+	play_sound_on_player(sound, default_player)
