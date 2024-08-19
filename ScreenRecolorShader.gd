37fee96784f7c6e25fa226a9d62fdf82c0d4d8a5
Nick Chrissomallis <nchrissomallis@comcast.net>
2024-08-18 23:30:44 -0400

    Enhancements to color replace shader to allow for easier expansion in the future.

--- ScreenRecolorShader.gd
+++ ScreenRecolorShader.gd
@@ -1,20 +1,29 @@
 extends CanvasLayer
 
 @onready var shader_material = $GBShader.get_material()
-@onready var pal_color_indices = range(1,5)
-@onready var pal_color = "GREEN"
+@onready var pal_color_indices = range(1,5):
+	set(value):
+		pal_color_indices = value
+		recalculate_screen_palettes()
+@onready var pal_color = "GREEN":
+	set(value):
+		pal_color = value
+		recalculate_screen_palettes()
 
 func _ready():
 	setup_color_replacement()
 
-func setup_color_replacement():
-	for n in range(1,5):
-		var RGBcolor = ProjectSettings.get_setting("shader_globals/SHADER_BASE"+str(n))
-		shader_material.set("shader_param/OLDCOLOR"+str(n), RGBcolor["value"])
-
 func set_screen_color(new_color):
 	pal_color = new_color
-	recalculate_screen_palettes()
+
+func reset_default_palette_range():
+	pal_color_indices = [1, 2, 3, 4]
+
+func blackout_screen():
+	pal_color_indices = [4, 4, 4, 4]
+
+func whiteout_screen():
+	pal_color_indices = [1, 1, 1, 1]
 
 func cycle_palettes_down():
 	var new_pal_color_indices
@@ -37,7 +46,6 @@ func cycle_palettes_down():
 	
 	if (new_pal_color_indices != pal_color_indices):
 		pal_color_indices = new_pal_color_indices
-		recalculate_screen_palettes()
 		
 func cycle_palettes_up():
 	var new_pal_color_indices
@@ -60,9 +68,20 @@ func cycle_palettes_up():
 	
 	if (new_pal_color_indices != pal_color_indices):
 		pal_color_indices = new_pal_color_indices
-		recalculate_screen_palettes()
+
+func setup_color_replacement():
+	var base_colors = []
+	for n in range(1,5):
+		var RGBcolor = ProjectSettings.get_setting("shader_globals/SHADER_BASE"+str(n))
+		base_colors.append(RGBcolor["value"]);
+		
+	shader_material.set("shader_parameter/OLD_COLORS", base_colors);
 
 func recalculate_screen_palettes():
+	var new_colors = []
 	for n in range(1,5):
 		var RGBcolor = ProjectSettings.get_setting("shader_globals/SHADER_"+pal_color+str(pal_color_indices[n-1]))
-		shader_material.set("shader_param/NEWCOLOR"+str(n), RGBcolor["value"])
+		new_colors.append(RGBcolor["value"]);
+	
+	shader_material.set("shader_parameter/NEW_COLORS", new_colors);
+	DisplayServer.set_native_icon("marty_icon_"+pal_color.to_lower()+".ico");
