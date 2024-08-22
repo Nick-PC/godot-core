51c25522968f9c08c8d55894fad56f43ed38a510
Nick Chrissomallis <nchrissomallis@comcast.net>
2024-08-21 23:21:27 -0400

    Added functionality to freeze sprite in SpriteFunctions

--- SpriteFunctions.gd
+++ SpriteFunctions.gd
@@ -3,7 +3,7 @@ extends Node
 func flash_sprite_n_times(sprite: AnimatedSprite2D, n: int):
 	await flash_sprites_n_times([sprite], n)
 func flash_sprites_n_times(sprites: Array[AnimatedSprite2D], n: int):
-	await flash_sprites_n_times_with_delay(sprites, n, 3)
+	await flash_sprites_n_times_with_delay(sprites, n, 2)
 
 func flash_sprite_n_times_with_delay(sprite: AnimatedSprite2D, n: int, delay_in_frames: int):
 	await flash_sprites_n_times_with_delay([sprite], n, delay_in_frames)
@@ -13,3 +13,11 @@ func flash_sprites_n_times_with_delay(sprites: Array[AnimatedSprite2D], n: int, 
 			sprite.visible = !sprite.visible
 		await TimerFunctions.wait_frames(delay_in_frames)
 	return
+
+func freeze_sprite(sprite: AnimatedSprite2D) -> void:
+	return freeze_sprites([sprite])
+func freeze_sprites(sprites: Array[AnimatedSprite2D]) -> void:
+	for sprite in sprites:
+		sprite.speed_scale = 0
+	return
+	
