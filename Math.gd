a1ac3b9d60cfa29319377886735a107452fcf4cf
Nick Chrissomallis <nchrissomallis@comcast.net>
2024-09-12 00:01:33 -0400

    Created functions to add/subtract to/from a value until it hits a max/min

--- Math.gd
+++ Math.gd
@@ -6,3 +6,17 @@ func normalize(value: float) -> int:
 		return 0
 	else:
 		return value / abs(value)
+
+func add_amount_to_until_extreme(value, amount, extreme):
+	if amount == 0:
+		return value
+	elif amount > 0:
+		return add_amount_until_maximum(value, amount, extreme)
+	else:
+		return subtract_amount_until_minimum(value, -1 * amount, extreme)
+
+func subtract_amount_until_minimum(value, amount, minimum):
+	return max(value - amount, minimum)
+
+func add_amount_until_maximum(value, amount, maximum):
+	return min(value + amount, maximum)
