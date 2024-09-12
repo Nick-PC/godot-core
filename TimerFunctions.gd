ba9c9648b1a5ba12d369195d7a85d6636ca65070
Nick Chrissomallis <nchrissomallis@comcast.net>
2024-09-12 18:59:43 -0400

    TimerFunctions: Add types to functions, create new function to reduce a timer down to a value (if below, do nothing, if stopped, start with value)

--- TimerFunctions.gd
+++ TimerFunctions.gd
@@ -1,11 +1,20 @@
 extends Node
 
-func seconds_from_frames(frames):
+func seconds_from_frames(frames: int) -> float:
 	return 1.0 * frames / ProjectSettings.get_setting("application/run/max_fps")
 
-func wait_frames(frames):
-	var seconds = seconds_from_frames(frames)
+func wait_frames(frames: int):
+	var seconds: float = seconds_from_frames(frames)
 	return wait_seconds(seconds)
 
-func wait_seconds(seconds):
+func wait_seconds(seconds: float):
 	return get_tree().create_timer(seconds).timeout
+
+func reduce_timer(timer: Timer, destination_amount: float) -> void:
+	if timer.is_stopped():
+		timer.start(destination_amount)
+	elif timer.time_left > destination_amount:
+		timer.start(destination_amount)
+
+func reduce_timer_to_default(timer: Timer) -> void:
+	reduce_timer(timer, timer.wait_time)
